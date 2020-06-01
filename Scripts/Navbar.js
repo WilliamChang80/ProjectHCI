6203a7bd28e9226d4d03a29c815680fba81faa1b
williamChang80 <william_chang80@rocketmail.com>
2020-06-01 23:33:25 +0700

    Added Carousel

--- Scripts/Navbar.js
+++ Scripts/Navbar.js
@@ -7,3 +7,19 @@ $("nav li").hover(
     $("ul", this).stop().slideUp(200);
   }
 );
+$("document").ready(function () {
+  $("#carousel").slick({
+    dots: true,
+    prevArrow: $("#prev"),
+    nextArrow: $("#next"),
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    initialSlide: 2,
+    infinite: true,
+    variableWidth: true,
+    variableHeight: true,
+    speed: 500
+  });
+});
