0b0375410bca01f727c81f984acb8790d6902edd
williamChang80 <william_chang80@rocketmail.com>
2020-06-04 23:26:54 +0700

    Changed Carousel Into Non Slick

--- Scripts/Home.js
+++ Scripts/Home.js
@@ -1,16 +1,29 @@
-$("document").ready(function () {
-  $("#carousel").slick({
-    dots: true,
-    prevArrow: $("#prev"),
-    nextArrow: $("#next"),
-    centerMode: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    initialSlide: 2,
-    infinite: true,
-    variableWidth: true,
-    variableHeight: true,
-    speed: 500
-  });
-});
+var slideIndex = 3;
+showSlides(slideIndex);
+
+function changeSlide(index) {
+  showSlides((slideIndex += index));
+}
+
+function currentSlide(index) {
+  showSlides((slideIndex = index));
+}
+
+function showSlides(index) {
+  var i;
+  var slides = $(".carousel-item");
+  var dots = $(".dot");
+  if (index > slides.length) {
+    slideIndex = 1;
+  }
+  if (index < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
