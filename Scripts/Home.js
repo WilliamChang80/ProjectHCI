b454c5ad030c553c201967b41790095e398cd39b
williamChang80 <william_chang80@rocketmail.com>
2020-06-04 15:07:24 +0700

    Removed Bootstrap

---
+++ Scripts/Home.js
@@ -0,0 +1,17 @@
+$("document").ready(function () {
+  $("#carousel").slick({
+    dots: true,
+    prevArrow: $("#prev"),
+    nextArrow: $("#next"),
+    centerMode: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    initialSlide: 2,
+    infinite: true,
+    variableWidth: true,
+    variableHeight: true,
+    speed: 500
+  });
+});
+console.log($(".carousel").attr("id"));
